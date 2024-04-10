26df1e9c38b430752b79a2c827f44acfa4a858b4
rzmhmd <86604045+rzmhmd@users.noreply.github.com>
2024-04-10 10:28:13 +0300

    Update pid_controller.cpp

--- pid_controller.cpp
+++ pid_controller.cpp
@@ -17,33 +17,33 @@ PID::~PID() {}
 
 void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, double output_lim_mini) {
    /**
-    TODO: Initialize PID coefficients (and errors, if needed)
+   * TODO: Initialize PID coefficients (and errors, if needed)
    **/
    Kp = Kpi;
    Ki = Kii;
    Kd = Kdi;
    output_lim_max = output_lim_maxi;
    output_lim_min = output_lim_mini;
+   dt = 0;
 }
 
 
 void PID::UpdateError(double cte) {
    /**
-    TODO: Update PID errors based on cte.
+   * TODO: Update PID errors based on cte.
    **/
-   diff_cte = (cte - prev_cte)/dt;
-   prev_cte = cte;
-   int_cte += cte*dt;
-   
+  P_cte = cte;
+  I_cte += cte;
+  D_cte = (cte-D_cte)/dt;
 }
 
 double PID::TotalError() {
    /**
-    TODO: Calculate and return the total error
+   * TODO: Calculate and return the total error
     * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
    */
     double control;
-    control = (prev_cte * Kp) + (int_cte * Ki) + (diff_cte * Kd);
+    control = Kp*P_cte + Ki*I_cte + Kd*D_cte;
     if (control > output_lim_max)
     {
       control = output_lim_max;
@@ -57,7 +57,7 @@ double PID::TotalError() {
 
 double PID::UpdateDeltaTime(double new_delta_time) {
    /**
-    TODO: Update the delta time with new value
+   * TODO: Update the delta time with new value
    */
    dt = new_delta_time;
    return dt;
